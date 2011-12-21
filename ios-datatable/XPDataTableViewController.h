ab95c9336732ed4febbaf6741c1ef04687904862
Bartek Wilczynski <bwilczyn@gmail.com>
2011-12-21 23:40:26 +0100

    [#1] Create controller to display data items in a table, closes #1

---
+++ ios-datatable/XPDataTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingGroupViewController.h
+//  
+//
+//  Created by Bartłomiej Wilczyński on 11/25/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XPDataTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *dataGroups;
+
+@end
---
+++ ios-datatable/XPDataTableViewController.m
@@ -0,0 +1,66 @@
+//
+//  SettingGroupViewController.m
+//  
+//
+//  Created by Bartłomiej Wilczyński on 11/25/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "XPDataTableViewController.h"
+#import "XPDataCell.h"
+#import "XPDataGroup.h"
+
+@implementation XPDataTableViewController
+
+@synthesize dataGroups;
+
+#pragma mark - Table view delegate
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    XPDataCell *cell = [[[self.dataGroups objectAtIndex:indexPath.section] cells] objectAtIndex:indexPath.row];
+    return cell.allowNavigation ? indexPath : nil;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    XPDataCell *cell = [[[self.dataGroups objectAtIndex:indexPath.section] cells] objectAtIndex:indexPath.row];
+    [cell tableViewRowSelected:tableView navigationController:self.navigationController];
+}
+
+#pragma mark - Table view data source
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure the cell...
+    XPDataGroup *group = [self.dataGroups objectAtIndex:indexPath.section];
+    XPDataCell *dataCell = [group.cells objectAtIndex:indexPath.row];
+    [dataCell configureCell:cell];
+    
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return [self.dataGroups count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [[[self.dataGroups objectAtIndex:section] items] count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return [[self.dataGroups objectAtIndex:section] text];
+}
+
+@end
