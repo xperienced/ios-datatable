60e732ce2e0f812bf13c72733991cba8763aa7ff
Bartek Wilczynski <bwilczyn@gmail.com>
2012-07-24 13:41:15 +0200

    Include all headers in a single file

---
+++ ios-datatable/XPDataTable.h
@@ -0,0 +1,23 @@
+//
+//  XPDataTable.h
+//  ios-datatable
+//
+//  Created by Bartłomiej Wilczyński on 7/24/12.
+//  Copyright (c) 2012 XPerienced. All rights reserved.
+//
+
+#ifndef ios_datatable_XPDataTable_h
+#define ios_datatable_XPDataTable_h
+
+#import "XPDataGroup.h"
+#import "XPDataCell.h"
+#import "XPSwitchDataCell.h"
+#import "XPSliderDataCell.h"
+#import "XPCheckmarkDataCell.h"
+#import "XPPickerDataCell.h"
+#import "XPDisclosureDataCell.h"
+
+#import "XPDataTableViewController.h"
+#import "XPListViewController.h"
+
+#endif
